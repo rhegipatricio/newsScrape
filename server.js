e459f441b3126014db1c5bf959149ba5af3c5d56
rhegipatricio <rhegi.patricio@gmail.com>
2017-07-20 17:39:56 -0400

    added code for public directory static

--- server.js
+++ server.js
@@ -10,3 +10,25 @@ var cheerio = require("cheerio");
 mongoose.Promise = Promise; 
 
 var app = express();
+
+app.use(logger("dev"));
+app.use(bodyParser.urlencoded({
+	extended: false
+}));
+
+// public dir static
+app.use(express.static("public"));
+
+// Database config with mongoose
+mongoose.connect("mongodb:")
+var db = mongoose.connection; 
+
+// Mongoose errors
+db.on("error", function(error) {
+	console.log("Mongoose Error: ", error);
+	)};
+
+// Once logged into the db through mongoose, success message shown 
+db.once("open", function() {
+	console.log("Mongoose connectino successful.");
+});
